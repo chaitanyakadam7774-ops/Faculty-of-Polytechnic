701c52c7a22e4a7d6e685572f7ce8363975dabde
chaitanyakadam7774-ops <chaitanyakadam7774@gmail.com>
2026-06-17 20:39:17 +0530

    Update and rename cp2 py to cp2.py

--- students/chaitanya-kadam/cp2.py
+++ students/chaitanya-kadam/cp2.py
@@ -13,4 +13,18 @@ def ask_text():
         else:
             return(sub_name)
             break
+def ask_marks():
+    user_input = input("Enter marks (1-100): ")
+    
+    if user_input.isdigit():
+        marks = int(user_input)
+        if 1 <= marks <= 100:
+            print(f"Success! You entered: {marks}")
+            return marks
+        else:
+            print("Error: Number must be between 1 and 100.")
+    else:
+         print("Error: Enter whole numbers only.")
+    return ask_marks()
+ask_marks()
   
